6addbbb359ccebcf7b731fe13822ce0ac3f2ed9b
Jakub Izydorczyk <j.izydorczyk@samsung.com>
2015-04-21 10:47:45 +0200

    Refactor widget handler
    Change-Id: I80c954a8c3a25732c38e66c248459c7187f33823

--- src/manifest_handlers/widget_handler.h
+++ src/manifest_handlers/widget_handler.h
@@ -16,25 +16,67 @@
 namespace wgt {
 namespace parse {
 
+class Preference {
+ public:
+  Preference(const std::string& name, const std::string& value,
+             bool readonly)
+      : name_(name),
+        value_(value),
+        readonly_(readonly) {}
+
+  std::string Name() const { return name_; }
+  std::string Value() const { return value_; }
+  bool ReadOnly() const { return readonly_; }
+ private:
+  std::string name_;
+  std::string value_;
+  bool readonly_;
+};
+
 class WidgetInfo : public parser::ManifestData {
  public:
   WidgetInfo();
   virtual ~WidgetInfo();
-  void SetString(const std::string& key, const std::string& value);
-  void Set(const std::string& key, parser::Value* value);
 
   // Name, short name and description are i18n items, they will be set
   // if their value were changed after loacle was changed.
-  void SetName(const std::string& name);
-  void SetShortName(const std::string& short_name);
-  void SetDescription(const std::string& description);
+  void set_name(const std::string& name);
+  void set_short_name(const std::string& short_name);
+  void set_description(const std::string& description);
 
-  parser::DictionaryValue* GetWidgetInfo() const {
-    return value_.get();
-  }
+  const std::string& name() const;
+  const std::string& short_name() const;
+  const std::string& description() const;
+
+  const std::string& id() const;
+  const std::string& version() const;
+  const std::string& view_modes() const;
+  const std::string& default_locale() const;
+  const std::string& author() const;
+  const std::string& author_email() const;
+  const std::string& author_href() const;
+  const std::string& widget_namespace() const;
+  const std::vector<Preference*>& preferences() const;
+  unsigned int height() const;
+  unsigned int width() const;
 
  private:
-  std::unique_ptr<parser::DictionaryValue> value_;
+  std::vector<Preference*> preferences_;
+  std::string id_;
+  std::string version_;
+  std::string viewmodes_;
+  std::string default_locale_;
+  std::string author_;
+  std::string description_;
+  std::string name_;
+  std::string short_name_;
+  std::string author_email_;
+  std::string author_href_;
+  std::string widget_namespace_;
+  unsigned int height_;
+  unsigned int width_;
+
+  friend class WidgetHandler;
 };
 
 class WidgetHandler : public parser::ManifestHandler {
