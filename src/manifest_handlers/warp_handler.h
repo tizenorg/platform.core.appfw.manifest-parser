f5208827b29fc9f308b26b3076329f5f7b814416
Wojciech Kosowicz <w.kosowicz@samsung.com>
2015-05-08 12:59:02 +0200

    [ManifestParser] Warp Handler
    Parsing of access element tag
    
    Change-Id: I96d98630a76cf0f7c5f98234827a5b56460f2884
    Signed-off-by: Wojciech Kosowicz <w.kosowicz@samsung.com>

---
+++ src/manifest_handlers/warp_handler.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE-xwalk file.
+
+#ifndef MANIFEST_HANDLERS_WARP_HANDLER_H_
+#define MANIFEST_HANDLERS_WARP_HANDLER_H_
+
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "manifest_parser/values.h"
+#include "manifest_parser/manifest_handler.h"
+
+namespace wgt {
+namespace parse {
+
+class WarpInfo : public parser::ManifestData {
+ public:
+  WarpInfo() {}
+  ~WarpInfo() override {}
+
+  void set_access_element(const std::pair<std::string, bool>& access_element) {
+    access_map_.push_back(access_element);
+  }
+
+  const std::vector<std::pair<std::string, bool> >& access_map() const {
+    return access_map_;
+  }
+
+ private:
+  std::vector<std::pair<std::string, bool> > access_map_;
+};
+
+class WarpHandler : public parser::ManifestHandler {
+ public:
+  WarpHandler() {}
+  ~WarpHandler() override {}
+
+  bool Parse(
+      const parser::Manifest& manifest,
+      std::shared_ptr<parser::ManifestData>* output,
+      std::string* error) override;
+  void ParseSingleAccessElement(
+      const parser::DictionaryValue& item_dict,
+      std::shared_ptr<WarpInfo> info);
+  void ParseAccessElements(
+      const parser::Manifest& manifest,
+      std::shared_ptr<WarpInfo> info);
+  std::string Key() const override;
+};
+}  // namespace parse
+}  // namespace wgt
+
+#endif  // MANIFEST_HANDLERS_WARP_HANDLER_H_
