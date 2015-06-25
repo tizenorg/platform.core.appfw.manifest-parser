ce85e97b80c5bcbdf72e4f8682a43754496b95ba
Tomasz Iwanek <t.iwanek@samsung.com>
2015-06-25 13:19:22 +0200

    Validate start file of widget
    This commit implement algorithm of finding start file within widget
    package. If there is no valid start file installation should fail.
    
     - http://www.w3.org/TR/widgets/#step-8-locate-the-start-file
     - http://www.w3.org/TR/widgets/#rule-for-finding-a-file-within-a-widget-package-0
    
    Fixes TCT widget02 testcases: b0, b5, c1, c2, c3, d9.
    
    Change-Id: Iff184a7b215ee840eda5b1f2635813fc249d85df

--- src/manifest_handlers/widget_config_parser.cc
+++ src/manifest_handlers/widget_config_parser.cc
@@ -4,6 +4,15 @@
 
 #include "manifest_handlers/widget_config_parser.h"
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <algorithm>
+#include <string>
 #include <vector>
 
 #include "manifest_handlers/app_control_handler.h"
@@ -22,6 +31,109 @@
 #include "manifest_handlers/widget_handler.h"
 #include "manifest_parser/manifest_handler.h"
 #include "manifest_parser/manifest_constants.h"
+#include "utils/logging.h"
+
+namespace ba = boost::algorithm;
+namespace bf = boost::filesystem;
+namespace bs = boost::system;
+
+namespace {
+
+const char kLocaleDirectory[] = "locales";
+
+const char* kDefaultStartFiles[] = {
+  "index.htm",
+  "index.html",
+  "index.svg",
+  "index.xhtml",
+  "index.xht"
+};
+
+enum class FindResult {
+  OK,
+  NUL,
+  ERROR
+};
+
+// http://www.w3.org/TR/widgets/#rule-for-finding-a-file-within-a-widget-package-0  // NOLINT
+FindResult FindFileWithinWidget(const bf::path& widget_path,
+    const std::string& content, bf::path* file = nullptr) {
+  std::string content_value = content;
+  if (content_value.empty())
+    return FindResult::ERROR;
+  if (content_value[0] == '/') {
+    content_value = content_value.substr(1);
+  }
+  // Steps 4. && 5.
+  std::vector<std::string> path_components;
+  ba::split(path_components, content_value, ba::is_any_of("/"));
+  if (path_components.size() >= 1 && path_components[0] == kLocaleDirectory) {
+    if (path_components.size() == 1) {
+      return FindResult::NUL;
+    }
+
+    // TODO(t.iwanek): validate language tag
+
+    path_components.erase(path_components.begin(), ++++path_components.begin());
+    content_value = ba::join(path_components, "/");
+  }
+  // for each locale in widget
+  // TODO(t.iwanek): this algorithm should accept input of preferred locale list
+  bs::error_code error;
+  bf::path locale_directory = widget_path / kLocaleDirectory;
+  if (bf::exists(locale_directory, error)) {
+    for (auto iter = bf::directory_iterator(locale_directory);
+         iter != bf::directory_iterator(); ++iter) {
+      const bf::path& path = *iter;
+
+      // TODO(t.iwanek): validate language tag
+
+      bf::path candidate = path / content_value;
+      if (bf::exists(candidate, error)) {
+        if (bf::is_directory(candidate, error)) {
+          return FindResult::ERROR;
+        }
+        if (file)
+          *file = candidate;
+        return FindResult::OK;
+      }
+    }
+  }
+
+  // default locale
+  bf::path root_candidate = widget_path / content_value;
+  if (bf::exists(root_candidate, error)) {
+    if (bf::is_directory(root_candidate, error)) {
+      return FindResult::ERROR;
+    }
+    if (file)
+      *file = root_candidate;
+    return FindResult::OK;
+  }
+
+  return FindResult::NUL;
+}
+
+// http://www.w3.org/TR/widgets/#step-8-locate-the-start-file
+bool CheckStartFileInWidget(const bf::path& widget_path,
+                            const std::string& content) {
+  if (!content.empty()) {
+    if (FindFileWithinWidget(widget_path, content) == FindResult::OK) {
+      LOG(INFO) << "Start file is: " << content;
+      return true;
+    }
+  }
+  for (auto& file : kDefaultStartFiles) {
+    if (FindFileWithinWidget(widget_path, file) == FindResult::OK) {
+      LOG(INFO) << "Start file is: " << file;
+      return true;
+    }
+  }
+  LOG(ERROR) << "No valid start file found";
+  return false;
+}
+
+}  // namespace
 
 namespace wgt {
 namespace parse {
@@ -62,12 +174,33 @@ const std::string& WidgetConfigParser::GetErrorMessage() const {
   return parser_->GetErrorMessage();
 }
 
+bool WidgetConfigParser::CheckStartFile() {
+  std::string content;
+  std::shared_ptr<const ContentInfo> content_info =
+      std::static_pointer_cast<const ContentInfo>(parser_->GetManifestData(
+          application_widget_keys::kTizenContentKey));
+  if (content_info) {
+    content = content_info->src();
+    if (content_info->is_tizen_content()) {
+      // external url in tizen:content is outside of w3c p&c spec
+      return true;
+    }
+  }
+
+  if (!CheckStartFileInWidget(widget_path_, content)) {
+    error_ = "Could not find valid start file";
+    return false;
+  }
+  return true;
+}
+
 bool WidgetConfigParser::ParseManifest(const boost::filesystem::path& path) {
   widget_path_ = path.parent_path();
   if (!parser_->ParseManifest(path))
     return false;
 
-  // TODO(t.iwanek): implement additional w3c checks!
+  if (!CheckStartFile())
+    return false;
 
   return true;
 }
