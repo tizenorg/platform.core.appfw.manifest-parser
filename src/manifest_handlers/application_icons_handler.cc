20f98756039c31eae949bbb198b24e5e85d3c7b1
Tomasz Iwanek <t.iwanek@samsung.com>
2015-07-07 10:42:03 +0200

    Parse icon height and width in ApplicationIconsHandler
    Although app-installer do not uses infomration about icon
    width and height, it can be seen in installer log to predict if
    following testcase would pass:
     - ix,
     - iy,
     - iz,
     - i1,
     - i2,
     - i3,
     - i4,
     - iq,
     - i9,
     - iw,
     - ie,
     - ir,
     - it,
     - ib.
    
    Must be merged and submitted together with app-installer change:
     - https://review.tizen.org/gerrit/#/c/43374/
    
    Change-Id: I26ce4703e623acdde132bb497f59df687adb2730

--- src/manifest_handlers/application_icons_handler.cc
+++ src/manifest_handlers/application_icons_handler.cc
@@ -9,33 +9,12 @@
 
 #include "utils/logging.h"
 
-namespace wgt {
-namespace parse {
-
 namespace keys = wgt::application_widget_keys;
 
-const std::vector<std::string>& ApplicationIconsInfo::get_icon_paths() const {
-  return icon_paths_;
-}
+namespace {
 
-void ApplicationIconsInfo::add_icon_path(const std::string& icon_path) {
-  // Eliminate duplicates, keep order
-  if (std::find(icon_paths_.begin(), icon_paths_.end(), icon_path)
-      != icon_paths_.end()) {
-    return;
-  }
-  icon_paths_.push_back(icon_path);
-}
-
-ApplicationIconsHandler::ApplicationIconsHandler() {
-}
-
-ApplicationIconsHandler::~ApplicationIconsHandler() {
-}
-
-bool ApplicationIconsHandler::ExtractIconSrc(
-    const parser::Value& dict, std::string* value,
-    std::string* error) {
+bool ExtractIconSrc(const parser::Value& dict, std::string* value,
+                    std::string* error) {
   const parser::DictionaryValue* inner_dict;
   if (!dict.GetAsDictionary(&inner_dict)) {
     LOG(INFO) << "Cannot get key value as a dictionary. Key name: widget.icon";
@@ -48,6 +27,103 @@ bool ApplicationIconsHandler::ExtractIconSrc(
   }
   *value = src;
   return  true;
+}
+
+/**
+ * @brief ExtractIconDimensions
+ *
+ * Parses and sets icon dimensions. If value cannot be received it is set to -1.
+ *
+ * @param dict dictionary value of icon
+ * @param height output parameter of height of icon
+ * @param width output parameter of width of icon
+ */
+void ExtractIconDimensions(const parser::Value& dict, int* height, int* width) {
+  *width = -1;
+  *height = -1;
+  const parser::DictionaryValue* inner_dict;
+  if (!dict.GetAsDictionary(&inner_dict)) {
+    return;
+  }
+
+  std::string width_str;
+  if (inner_dict->GetString(keys::kWidgetIconWidthKey, &width_str)) {
+    try {
+      *width = std::stoi(width_str);
+    } catch (const std::logic_error&) {
+       // ignore, width is set to -1
+    }
+  }
+
+  std::string height_str;
+  if (inner_dict->GetString(keys::kWidgetIconHeightKey, &height_str)) {
+    try {
+      *height = std::stoi(height_str);
+    } catch (const std::logic_error&) {
+      // ignore, height is set to -1
+    }
+  }
+}
+
+}  // namespace
+
+namespace wgt {
+namespace parse {
+
+ApplicationIcon::ApplicationIcon(const std::string& path, int height, int width)
+    : path_(path),
+      height_((height >= 0) ? height: -1),
+      width_((width >= 0) ? width: -1) {
+}
+
+bool ApplicationIcon::GetWidth(int* width) const {
+  if (!width || width_ < 0)
+    return false;
+  *width = width_;
+  return true;
+}
+
+bool ApplicationIcon::GetHeight(int* height) const {
+  if (!height || height_ < 0)
+    return false;
+  *height = height_;
+  return true;
+}
+
+const std::string& ApplicationIcon::path() const {
+  return path_;
+}
+
+void ApplicationIcon::set_width(int width) {
+  if (width >= 0)
+    width_ = width;
+}
+
+void ApplicationIcon::set_height(int height) {
+  if (height >= 0)
+    height_ = height;
+}
+
+const std::vector<ApplicationIcon>& ApplicationIconsInfo::icons() const {
+  return icons_;
+}
+
+void ApplicationIconsInfo::AddIcon(const ApplicationIcon& new_icon) {
+  // Eliminate duplicates, keep order
+  if (std::find_if(icons_.begin(), icons_.end(),
+      [&new_icon](const ApplicationIcon& icon) {
+        return icon.path() == new_icon.path();
+      })
+      != icons_.end()) {
+    return;
+  }
+  icons_.push_back(new_icon);
+}
+
+ApplicationIconsHandler::ApplicationIconsHandler() {
+}
+
+ApplicationIconsHandler::~ApplicationIconsHandler() {
 }
 
 bool ApplicationIconsHandler::Parse(
@@ -63,12 +139,15 @@ bool ApplicationIconsHandler::Parse(
   }
 
   if (key_value->IsType(parser::Value::TYPE_DICTIONARY)) {
-    std::string icon;
-    if (!ExtractIconSrc(*key_value, &icon, error)) {
+    std::string icon_path;
+    int width = -1;
+    int height = -1;
+    if (!ExtractIconSrc(*key_value, &icon_path, error)) {
       *error = "Cannot get key value as a dictionary. Key name: widget.icon";
       return false;
     }
-    app_icons_info->add_icon_path(icon);
+    ExtractIconDimensions(*key_value, &height, &width);
+    app_icons_info->AddIcon(ApplicationIcon(icon_path, height, width));
   } else if (key_value->IsType(parser::Value::TYPE_LIST)) {
     const parser::ListValue* list;
     if (!key_value->GetAsList(&list)) {
@@ -76,13 +155,16 @@ bool ApplicationIconsHandler::Parse(
       return false;
     }
     for (const parser::Value* list_value : *list) {
-      std::string icon;
-      if (!ExtractIconSrc(*list_value, &icon, error)) {
+      std::string icon_path;
+      int width = -1;
+      int height = -1;
+      if (!ExtractIconSrc(*list_value, &icon_path, error)) {
         *output =
             std::static_pointer_cast<parser::ManifestData>(app_icons_info);
         return true;
       }
-      app_icons_info->add_icon_path(icon);
+      ExtractIconDimensions(*key_value, &height, &width);
+      app_icons_info->AddIcon(ApplicationIcon(icon_path, height, width));
     }
   }
   *output = std::static_pointer_cast<parser::ManifestData>(app_icons_info);
