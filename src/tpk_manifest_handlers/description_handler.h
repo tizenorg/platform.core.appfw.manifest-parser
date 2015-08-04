53dab2fdb3c3c9ba1946e31ff2029db26dc57490
Maciej Socha <m.socha@samsung.com>
2015-08-04 16:57:30 +0200

    [Parser unification] description_handler
    Handler for "description" element of tpk manifest file.
    
    Change-Id: Ifb31811d3e7897828f12f6f4d31834d2a641eed9

---
+++ src/tpk_manifest_handlers/description_handler.h
@@ -0,0 +1,63 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef TPK_MANIFEST_HANDLERS_DESCRIPTION_HANDLER_H_
+#define TPK_MANIFEST_HANDLERS_DESCRIPTION_HANDLER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "manifest_parser/manifest_handler.h"
+#include "manifest_parser/values.h"
+#include "tpk_manifest_handlers/application_manifest_constants.h"
+
+namespace tpk {
+namespace parse {
+
+class DescriptionInfo : public parser::ManifestData {
+ public:
+  DescriptionInfo(const std::string& description,
+                  const std::string& xml_lang)
+                  : description_(description),
+                    xml_lang_(xml_lang) {}
+  ~DescriptionInfo() override {}
+
+  const std::string& description() const {
+    return description_;
+  }
+  // NOTE: not in spec
+  const std::string& xml_lang() const {
+    return xml_lang_;
+  }
+
+ private:
+  std::string description_;
+  std::string xml_lang_;
+};
+
+struct DescriptionInfoList : public parser::ManifestData {
+  std::vector<DescriptionInfo> items;
+};
+
+class DescriptionHandler : public parser::ManifestHandler {
+ public:
+  DescriptionHandler() {}
+  ~DescriptionHandler() override {}
+
+  bool Parse(
+      const parser::Manifest& manifest,
+      std::shared_ptr<parser::ManifestData>* output,
+      std::string* error) override;
+  bool Validate(
+      const parser::ManifestData& data,
+      const parser::ManifestDataMap& handlers_output,
+      std::string* error) const override;
+  std::string Key() const override;
+};
+
+}   // namespace parse
+}   // namespace tpk
+
+#endif  // TPK_MANIFEST_HANDLERS_DESCRIPTION_HANDLER_H_
