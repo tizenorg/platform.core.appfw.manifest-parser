4ff729f9fd4f356f554317f3dc6695955e1a2415
Maciej Socha <m.socha@samsung.com>
2015-08-07 14:44:25 +0200

    [Parser unification] privileges_handler
    Handler for "privileges" element of tpk manifest file.
    
    Change-Id: I6d81a08b1015e720fc801612bfa95d102ee97856

---
+++ src/tpk_manifest_handlers/privileges_handler.cc
@@ -0,0 +1,81 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include "tpk_manifest_handlers/privileges_handler.h"
+
+#include <utility>
+
+#include "manifest_parser/manifest_util.h"
+#include "manifest_parser/values.h"
+#include "utils/iri_util.h"
+#include "utils/logging.h"
+#include "tpk_manifest_handlers/application_manifest_constants.h"
+
+// TODO(m.socha): Handler is very similar to wgt permissions_handler.
+//                It should be refactored.
+
+namespace tpk {
+namespace parse {
+
+namespace keys = tpk::application_keys;
+
+bool PrivilegesHandler::Parse(
+    const parser::Manifest& manifest,
+    std::shared_ptr<parser::ManifestData>* output,
+    std::string* error) {
+  std::shared_ptr<PrivilegesInfo> privileges_info(new PrivilegesInfo());
+  parser::Value* value = nullptr;
+  if (!manifest.Get(keys::kPrivilegeKey, &value))
+    return true;
+
+  std::unique_ptr<parser::ListValue> privileges_list;
+  if (value->IsType(parser::Value::TYPE_DICTIONARY)) {
+    privileges_list.reset(new parser::ListValue);
+    privileges_list->Append(value->DeepCopy());
+  } else {
+    parser::ListValue* list = nullptr;
+    value->GetAsList(&list);
+    if (list)
+      privileges_list.reset(list->DeepCopy());
+  }
+
+  if (!privileges_list) {
+    *error = "Invalid value of privileges.";
+    return false;
+  }
+  for (parser::ListValue::const_iterator it = privileges_list->begin();
+       it != privileges_list->end(); ++it) {
+    parser::DictionaryValue* dictionary_value = nullptr;
+    (*it)->GetAsDictionary(&dictionary_value);
+
+    std::string privilege;
+    if (!dictionary_value ||
+        !dictionary_value->GetString(
+            keys::kPrivilegeTextKey, &privilege) ||
+        privilege.empty())
+      continue;
+
+    privileges_info->AddPrivilege(privilege);
+  }
+
+  *output = std::static_pointer_cast<parser::ManifestData>(privileges_info);
+  return true;
+}
+
+bool PrivilegesHandler::Validate(
+    const parser::ManifestData& data,
+    const parser::ManifestDataMap& /*handlers_output*/,
+    std::string* /*error*/) const {
+  const PrivilegesInfo& elements =
+       static_cast<const PrivilegesInfo&>(data);
+  // TODO(m.socha): There should be 'privileges' validation.
+  return true;
+}
+
+std::string PrivilegesHandler::Key() const {
+  return keys::kPrivilegeKey;
+}
+
+}   // namespace parse
+}   // namespace tpk
---
+++ src/tpk_manifest_handlers/privileges_handler.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef TPK_MANIFEST_HANDLERS_PRIVILEGES_HANDLER_H_
+#define TPK_MANIFEST_HANDLERS_PRIVILEGES_HANDLER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "manifest_parser/manifest_handler.h"
+#include "manifest_parser/permission_types.h"
+#include "manifest_parser/values.h"
+#include "tpk_manifest_handlers/application_manifest_constants.h"
+
+namespace tpk {
+namespace parse {
+
+class PrivilegesInfo : public parser::ManifestData {
+ public:
+  const parser::PrivilegesSet& GetPrivileges() const {
+    return privileges_;
+  }
+
+  void AddPrivilege(const std::string& privilege) {
+    privileges_.insert(privilege);
+  }
+
+ private:
+  parser::PrivilegesSet privileges_;
+};
+
+class PrivilegesHandler : public parser::ManifestHandler {
+ public:
+  bool Parse(
+      const parser::Manifest& manifest,
+      std::shared_ptr<parser::ManifestData>* output,
+      std::string* error) override;
+  bool Validate(
+      const parser::ManifestData& data,
+      const parser::ManifestDataMap& handlers_output,
+      std::string* error) const override;
+  std::string Key() const override;
+};
+
+}   // namespace parse
+}   // namespace tpk
+
+#endif  // TPK_MANIFEST_HANDLERS_PRIVILEGES_HANDLER_H_
