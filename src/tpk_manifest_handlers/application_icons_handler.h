68e98ce32f70bdb1aaf131c576e04c7ed30da35b
Maciej Socha <m.socha@samsung.com>
2015-08-10 11:06:07 +0200

    [Parser unification] application_icons_handler
    Handler for "icon" element of tpk manifest file.
    
    Change-Id: Iddf5b6bbed2214a60bded3bb416eeaa89c955288

---
+++ src/tpk_manifest_handlers/application_icons_handler.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef TPK_MANIFEST_HANDLERS_APPLICATION_ICONS_HANDLER_H_
+#define TPK_MANIFEST_HANDLERS_APPLICATION_ICONS_HANDLER_H_
+
+#include <string>
+#include <vector>
+
+#include "manifest_parser/manifest_handler.h"
+#include "tpk_manifest_handlers/application_manifest_constants.h"
+
+namespace tpk {
+namespace parse {
+
+class ApplicationIcon {
+ public:
+  explicit ApplicationIcon(const std::string& path)
+                          : path_(path) {}
+  ~ApplicationIcon() {}
+
+const std::string& path() const {
+  return path_;
+}
+
+ private:
+  std::string path_;
+};
+
+class ApplicationIconsInfo : public parser::ManifestData {
+ public:
+  const std::vector<ApplicationIcon>& icons() const {
+  return icons_;
+}
+
+  void AddIcon(const ApplicationIcon& icon_path);
+
+ private:
+  std::vector<ApplicationIcon> icons_;
+};
+
+class ApplicationIconsHandler : public parser::ManifestHandler {
+ public:
+  ApplicationIconsHandler();
+  ~ApplicationIconsHandler();
+
+  bool Parse(
+      const parser::Manifest& manifest,
+      std::shared_ptr<parser::ManifestData>* output,
+      std::string* error) override;
+  bool AlwaysParseForType() const override;
+  std::string Key() const override;
+};
+
+}  // namespace parse
+}  // namespace tpk
+
+#endif  // TPK_MANIFEST_HANDLERS_APPLICATION_ICONS_HANDLER_H_
