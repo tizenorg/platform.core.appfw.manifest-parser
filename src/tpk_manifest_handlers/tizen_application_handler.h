e53832d4d18ec5b6a590bfd40a677da93dec0640
Maciej Socha <m.socha@samsung.com>
2015-08-21 09:43:27 +0200

    [Parser unification] tizen_application_handler
    Handler for "manifest" element of tpk manifest file.
    
    Change-Id: I1af0cf9e49cde00b41f3bf52884d2dfa1f5c3f6e

---
+++ src/tpk_manifest_handlers/tizen_application_handler.h
@@ -0,0 +1,77 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef TPK_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
+#define TPK_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
+
+#include <memory>
+#include <string>
+
+#include "manifest_parser/manifest_handler.h"
+#include "manifest_parser/values.h"
+#include "tpk_manifest_handlers/application_manifest_constants.h"
+
+namespace tpk {
+namespace parse {
+
+class TizenApplicationInfo : public parser::ManifestData {
+ public:
+  void set_xmlns(const std::string& xmlns) {
+    xmlns_ = xmlns;
+  }
+  void set_api_version(const std::string& api_version) {
+    api_version_ = api_version;
+  }
+  void set_package(const std::string& package) {
+    package_ = package;
+  }
+  void set_version(const std::string& version) {
+    version_ = version;
+  }
+  void set_install_location(const std::string& install_location) {
+    install_location_ = install_location;
+  }
+
+  const std::string& xmlns() const {
+    return xmlns_;
+  }
+  const std::string& api_version() const {
+    return api_version_;
+  }
+  const std::string& package() const {
+    return package_;
+  }
+  const std::string& version() const {
+    return version_;
+  }
+  const std::string& install_location() const {
+    return install_location_;
+  }
+
+
+ private:
+  std::string xmlns_;
+  std::string api_version_;
+  std::string package_;
+  std::string version_;
+  std::string install_location_;
+};
+
+class TizenApplicationHandler : public parser::ManifestHandler {
+ public:
+  bool Parse(
+      const parser::Manifest& manifest,
+      std::shared_ptr<parser::ManifestData>* output,
+      std::string* error) override;
+  bool Validate(
+      const parser::ManifestData& data,
+      const parser::ManifestDataMap& handlers_output,
+      std::string* error) const override;
+  std::string Key() const override;
+};
+
+}   // namespace parse
+}   // namespace tpk
+
+#endif  // TPK_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
