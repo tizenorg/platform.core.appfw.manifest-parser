fd9e4464d440a7d86aed265275d3a0cd5b6f804c
Wojciech Kosowicz <w.kosowicz@samsung.com>
2015-09-30 11:35:33 +0200

    SplashScreen
    Full handler implementation
    
    Change-Id: Ib084bbc250dda71afd79e1f51e290a77172ebf0b

--- src/manifest_handlers/splash_screen_handler.h
+++ src/manifest_handlers/splash_screen_handler.h
@@ -9,24 +9,57 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
+#include "manifest_handlers/setting_handler.h"
 #include "manifest_parser/manifest_handler.h"
 #include "manifest_parser/values.h"
 
 namespace wgt {
 namespace parse {
 
+enum class ReadyWhen { FIRSTPAINT, USERINTERACTIVE, COMPLETE, CUSTOM };
+
+enum class ScreenOrientation { AUTO, PORTRAIT, LANDSCAPE, NONE };
+
+struct Color {
+  unsigned char red;
+  unsigned char green;
+  unsigned char blue;
+};
+
+struct SplashScreenData {
+  ScreenOrientation orientation;
+  Color color;
+  std::vector<std::string> background_color;
+  std::vector<std::string> background_image;
+  std::vector<std::string> image;
+  std::vector<std::string> image_border;
+};
+
 class SplashScreenInfo : public parser::ManifestData {
  public:
-  SplashScreenInfo();
-  virtual ~SplashScreenInfo();
+  SplashScreenInfo() {}
+  virtual ~SplashScreenInfo() {}
 
-  void set_src(const std::string &src) { src_ = src; }
+  void set_src(const std::string& src) { src_ = src; }
   const std::string& src() const { return src_; }
+  void set_splash_screen_data(
+      std::pair<ScreenOrientation, SplashScreenData> splash_screen_data) {
+    splash_screen_data_.insert(splash_screen_data);
+  }
+  const std::map<ScreenOrientation, SplashScreenData>& splash_screen_data()
+      const {
+    return splash_screen_data_;
+  }
+  void set_ready_when(ReadyWhen ready_when) { ready_when_ = ready_when; }
+  const ReadyWhen ready_when() const { return ready_when_; }
 
  private:
   std::string src_;
+  std::map<ScreenOrientation, SplashScreenData> splash_screen_data_;
+  ReadyWhen ready_when_;
 };
 
 /**
@@ -37,18 +70,27 @@ class SplashScreenInfo : public parser::ManifestData {
  */
 class SplashScreenHandler : public parser::ManifestHandler {
  public:
-  SplashScreenHandler();
-  virtual ~SplashScreenHandler();
-
-  bool Parse(
-      const parser::Manifest& manifest,
-      std::shared_ptr<parser::ManifestData>* output,
-      std::string* error) override;
-  bool Validate(
-      const parser::ManifestData& data,
-      const parser::ManifestDataMap& handlers_output,
-      std::string* error) const override;
+  SplashScreenHandler() {}
+  virtual ~SplashScreenHandler() {}
+  bool Parse(const parser::Manifest& manifest,
+             std::shared_ptr<parser::ManifestData>* output,
+             std::string* error) override;
+  bool Validate(const parser::ManifestData& data,
+                const parser::ManifestDataMap& handlers_output,
+                std::string* error) const override;
   std::string Key() const override;
+
+ private:
+  bool ParseSingleOrientation(const parser::Manifest& manifest,
+                              ScreenOrientation orientation,
+                              SplashScreenInfo* ss_info);
+  bool ParseElement(const parser::DictionaryValue* dict,
+                    std::vector<std::string>* to_be_saved,
+                    const char* keyToParse);
+  bool ParseReadyWhen(const parser::Manifest& manifest,
+                      SplashScreenInfo* ss_info);
+  bool ParseColor(const parser::DictionaryValue* dict,
+                  SplashScreenData* splash_screen);
 };
 
 }  // namespace parse
