416b27cac40d83badd1ef1f42d6695a3aa33aca1
Lukasz Wysocki <l.wysocki@samsung.com>
2015-12-15 10:49:46 +0100

    Add background-category parsing from metadata element
    Refactoring UI, Service and Widget Application Handlers
    
    This commit is required by following change:
    - https://review.tizen.org/gerrit/#/c/54915/
    
    This change requires:
    - https://review.tizen.org/gerrit/#/c/55660/
    - https://review.tizen.org/gerrit/#/c/55192/
    
    Add support for modifications from following change:
    - https://review.tizen.org/gerrit/#/c/54771/
    
    Change-Id: I65d5617f80a2f426ca297b5ca9ad22ce910c2262

--- src/manifest_parser/utils/iri_util.h
+++ src/manifest_parser/utils/iri_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a apache 2.0 license that can be
 // found in the LICENSE file.
 
-#ifndef UTILS_IRI_UTIL_H_
-#define UTILS_IRI_UTIL_H_
+#ifndef MANIFEST_PARSER_UTILS_IRI_UTIL_H_
+#define MANIFEST_PARSER_UTILS_IRI_UTIL_H_
 
 #include <string>
 
@@ -24,4 +24,4 @@ bool IsValidIRI(const std::string& iri_string);
 }  // namespace utils
 }  // namespace parser
 
-#endif  // UTILS_IRI_UTIL_H_
+#endif  // MANIFEST_PARSER_UTILS_IRI_UTIL_H_
