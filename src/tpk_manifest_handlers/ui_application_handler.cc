13ccd48799b87be8db1f8f12e0cf5ce949be9733
Lukasz Wysocki <l.wysocki@samsung.com>
2015-12-15 10:49:46 +0100

    Add background-category parsing from metadata element
    Refactoring UI, Service and Widget Application Handlers
    
    This commit is required by following change:
    - https://review.tizen.org/gerrit/#/c/54915/
    
    This change requires:
    - https://review.tizen.org/gerrit/#/c/55660/
    
    Add support for modifications from following change:
    - https://review.tizen.org/gerrit/#/c/54771/
    
    Change-Id: I65d5617f80a2f426ca297b5ca9ad22ce910c2262

--- src/tpk_manifest_handlers/ui_application_handler.cc
+++ src/tpk_manifest_handlers/ui_application_handler.cc
@@ -9,19 +9,11 @@
 #include <utility>
 
 #include "manifest_parser/manifest_util.h"
+#include "manifest_parser/utils/logging.h"
+#include "manifest_parser/utils/version_number.h"
 #include "manifest_parser/values.h"
-#include "utils/iri_util.h"
-#include "utils/logging.h"
-#include "utils/version_number.h"
 #include "tpk_manifest_handlers/application_manifest_constants.h"
 #include "tpk_manifest_handlers/package_handler.h"
-
-namespace {
-
-const utils::VersionNumber kLaunchModeRequiredVersion("2.4");
-const char kUIApplicationKey[] = "manifest.ui-application";
-
-}  // namespace
 
 namespace tpk {
 namespace parse {
@@ -30,42 +22,11 @@ namespace ba = boost::algorithm;
 namespace keys = tpk::application_keys;
 
 namespace {
-// app-control
-const char kAppControlKey[] = "app-control";
-const char kAppControlOperationKey[] = "operation";
-const char kAppControlURIKey[] = "uri";
-const char kAppControlMimeKey[] = "mime";
-const char kAppControlNameChildKey[] = "@name";
-
-// background-category
-const char kBackgroundCategoryKey[] = "background-category";
-const char kBackgroundCategoryValueKey[] = "@value";
-
-// datacontrol
-const char kDataControlKey[] = "datacontrol";
-const char kDataControlAccessKey[] = "@access";
-const char kDataControlProviderIDKey[] = "@providerid";
-const char kDataControlTypeKey[] = "@type";
-
-// icon
-const char kIconKey[] = "icon";
-const char kIconTextKey[] = "#text";
-
 // image
 const char kImageKey[] = "image";
 const char kImageNameKey[] = "@name";
 const char kImageSectionKey[] = "@section";
 const char kImageLangKey[] = "@lang";
-
-// label
-const char kLabelKey[] = "label";
-const char kLabelTextKey[] = "#text";
-const char kLabelLangKey[] = "@lang";
-
-// metadata
-const char kMetaDataKey[] = "metadata";
-const char kMetaDataKeyKey[] = "@key";
-const char kMetaDataValueKey[] = "@value";
 
 // ui-application
 const char kUIApplicationAppIDKey[] = "@appid";
@@ -95,229 +56,85 @@ bool IsBooleanString(const std::string& value) {
 
 }  // namespace
 
-bool ParseAppControl(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
-  for (const auto& itemOperation : parser::GetOneOrMany(dict,
-      kAppControlOperationKey, "")) {
-    std::string operation;
-    std::string uri;
-    std::string mime;
-    itemOperation->GetString(kAppControlNameChildKey, &operation);
-
-    for (const auto& itemURI : parser::GetOneOrMany(dict,
-        kAppControlURIKey, "")) {
-      itemURI->GetString(kAppControlNameChildKey, &uri);
-
-      for (const auto& itemMime : parser::GetOneOrMany(dict,
-          kAppControlMimeKey, "")) {
-        itemMime->GetString(kAppControlNameChildKey, &mime);
-        info->app_control.emplace_back(operation, uri, mime);
-      }
-    }
-  }
-
-  return true;
-}
-
-bool ParseBackgroundCategoryElement(
-    const parser::DictionaryValue* dict,
-    UIApplicationSingleEntry* info) {
-  std::string value;
-
-  if (!dict->GetString(kBackgroundCategoryValueKey, &value))
-    return false;
-
-  info->background_category.emplace_back(std::move(value));
-
-  return true;
-}
-
-bool ParseDataControl(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
-  std::string access;
-  dict->GetString(kDataControlAccessKey, &access);
-  std::string providerid;
-  dict->GetString(kDataControlProviderIDKey, &providerid);
-  std::string type;
-  dict->GetString(kDataControlTypeKey, &type);
-  info->data_control.emplace_back(access, providerid, type);
-  return true;
-}
-
-bool ParseMetaData(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
-  std::string key;
-  dict->GetString(kMetaDataKeyKey, &key);
-  std::string val;
-  dict->GetString(kMetaDataValueKey, &val);
-  info->meta_data.emplace_back(key, val);
-  return true;
-}
-
-bool ParseAppIcon(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
+template<>
+bool ParseAppIcon(const parser::DictionaryValue& dict,
+                  UIApplicationSingleEntry* info, std::string*) {
   std::string icon_path;
-  if (!dict->GetString(kIconTextKey, &icon_path))
-    return false;
+
+  if (!dict.GetString(tpk_app_keys::kIconTextKey, &icon_path))
+    return true;
+
   info->app_icons.AddIcon(ApplicationIcon(icon_path));
+
   return true;
 }
 
-bool ParseAppImage(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
+bool ParseAppImage(const parser::DictionaryValue& dict,
+                   UIApplicationSingleEntry* info, std::string*) {
   std::string image_name;
   std::string image_section;
   std::string image_lang;
-  if (!dict->GetString(kImageNameKey, &image_name))
-    return false;
-  dict->GetString(kImageSectionKey, &image_section);
-  dict->GetString(kImageLangKey, &image_lang);
+
+  if (!dict.GetString(kImageNameKey, &image_name))
+    return true;
+
+  dict.GetString(kImageSectionKey, &image_section);
+  dict.GetString(kImageLangKey, &image_lang);
   info->app_images.images.emplace_back(image_name, image_section, image_lang);
-  return true;
-}
 
-bool ParseLabel(
-  const parser::DictionaryValue* dict,
-  UIApplicationSingleEntry* info) {
-  std::string text;
-  dict->GetString(kLabelTextKey, &text);
-  std::string xml_lang;
-  dict->GetString(kLabelLangKey, &xml_lang);
-  info->label.emplace_back(text, text, xml_lang);
-  return true;
-}
-
-bool InitializeAppControlParsing(
-    const parser::DictionaryValue& control_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&control_dict, kAppControlKey, "")) {
-    if (!ParseAppControl(item, uiapplicationinfo)) {
-      *error = "Parsing AppControl failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeBackgroundCategoryParsing(
-    const parser::DictionaryValue& control_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&control_dict,
-      kBackgroundCategoryKey, "")) {
-    if (!ParseBackgroundCategoryElement(item, uiapplicationinfo)) {
-      *error = "Parsing background-category element failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeDataControlParsing(
-    const parser::DictionaryValue& control_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&control_dict, kDataControlKey, "")) {
-    if (!ParseDataControl(item, uiapplicationinfo)) {
-      *error = "Parsing DataControl failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeMetaDataParsing(
-    const parser::DictionaryValue& app_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&app_dict, kMetaDataKey, "")) {
-    if (!ParseMetaData(item, uiapplicationinfo)) {
-      *error = "Parsing Metadata failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeIconParsing(
-    const parser::DictionaryValue& app_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&app_dict, kIconKey, "")) {
-    if (!ParseAppIcon(item, uiapplicationinfo)) {
-      *error = "Parsing Icon failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeImageParsing(
-    const parser::DictionaryValue& app_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&app_dict, kImageKey, "")) {
-    if (!ParseAppImage(item, uiapplicationinfo)) {
-      *error = "Parsing Image failed";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool InitializeLabelParsing(
-    const parser::DictionaryValue& control_dict,
-    UIApplicationSingleEntry* uiapplicationinfo,
-    std::string* error) {
-  for (auto& item : parser::GetOneOrMany(&control_dict, kLabelKey, "")) {
-    if (!ParseLabel(item, uiapplicationinfo)) {
-      *error = "Parsing Label failed";
-      return false;
-    }
-  }
   return true;
 }
 
 bool InitializeParsing(const parser::DictionaryValue& app_dict,
                        UIApplicationSingleEntry* uiapplicationinfo,
                        std::string* error) {
-  if (!InitializeAppControlParsing(app_dict, uiapplicationinfo, error))
+  ParsingFuncPtr<UIApplicationSingleEntry> parsingFunc =
+      ParseAppControl<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kAppControlKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeDataControlParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseDataControl<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kDataControlKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeMetaDataParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseMetaData<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kMetaDataKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeIconParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseAppIcon<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kIconKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeLabelParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseLabel<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kLabelKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeImageParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseAppImage;
+  if (!InitializeParsingElement(app_dict, kImageKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
-  if (!InitializeBackgroundCategoryParsing(app_dict, uiapplicationinfo, error))
+  parsingFunc = ParseBackgroundCategoryElement<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kBackgroundCategoryKey,
+      parsingFunc, uiapplicationinfo, error))
     return false;
+
   return true;
 }
 
 bool UIAppValidation(const UIApplicationSingleEntry& item,
                      const std::string& api_version, std::string* error) {
-  if (item.ui_info.appid().empty()) {
+  if (item.app_info.appid().empty()) {
     *error = "The appid child element of ui-application element is obligatory";
     return false;
   }
 
-  const std::string& exec = item.ui_info.exec();
+  const std::string& exec = item.app_info.exec();
   if (exec.empty()) {
     *error = "The exec child element of ui-application element is obligatory";
     return false;
   }
 
-  const std::string& launch_mode = item.ui_info.launch_mode();
+  const std::string& launch_mode = item.app_info.launch_mode();
   if (!launch_mode.empty()) {
     if (utils::VersionNumber(api_version) < kLaunchModeRequiredVersion) {
       *error = "launch_mode attribute cannot be used for api version lower"
@@ -332,25 +149,25 @@ bool UIAppValidation(const UIApplicationSingleEntry& item,
     }
   } else {
     // FIXME currently const_cast used, but it is not the best way.
-    UIApplicationInfo &tmp = const_cast<UIApplicationInfo &>(item.ui_info);
+    UIApplicationInfo &tmp = const_cast<UIApplicationInfo &>(item.app_info);
     tmp.set_launch_mode("single");
   }
 
-  const std::string& multiple = item.ui_info.multiple();
+  const std::string& multiple = item.app_info.multiple();
   if (multiple.empty()) {
     *error =
         "The multiple child element of ui-application element is obligatory";
     return false;
   }
 
-  const std::string& nodisplay = item.ui_info.nodisplay();
+  const std::string& nodisplay = item.app_info.nodisplay();
   if (nodisplay.empty()) {
     *error =
         "The nodisplay child element of ui-application element is obligatory";
     return false;
   }
 
-  const std::string& taskmanage = item.ui_info.taskmanage();
+  const std::string& taskmanage = item.app_info.taskmanage();
   if (taskmanage.empty()) {
     *error =
         "The taskmanage child element of "
@@ -358,96 +175,12 @@ bool UIAppValidation(const UIApplicationSingleEntry& item,
     return false;
   }
 
-  const std::string& type = item.ui_info.type();
+  const std::string& type = item.app_info.type();
   if (type.empty()) {
     *error =
         "The type child element of "
         "ui-application element is obligatory";
     return false;
-  }
-  return true;
-}
-
-bool AppControlValidation(
-    const UIApplicationSingleEntry& it,
-    std::string* error) {
-  for (const auto& item : it.app_control) {
-    if (item.operation().empty()) {
-      *error =
-          "The operation child element of app-control element is obligatory";
-      return false;
-    } else if (!parser::utils::IsValidIRI(item.operation())) {
-      *error =
-          "The operation child element of app-control element is not valid url";
-      return false;
-    }
-    if (item.uri().empty()) {
-      *error =
-          "The mime child element of app-control element is obligatory";
-      return false;
-    }
-    if (item.mime().empty()) {
-      *error =
-          "The mime child element of app-control element is obligatory";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool DataControlValidation(
-    const UIApplicationSingleEntry& it,
-    std::string* error) {
-  for (const auto& item : it.data_control) {
-    if (item.access().empty()) {
-      *error =
-          "The access child element of datacontrol element is obligatory";
-      return false;
-    }
-
-    const std::string& providerid = item.providerid();
-    if (providerid.empty()) {
-      *error =
-          "The providerid child element of datacontrol element is obligatory";
-      return false;
-    }
-
-    const std::string& type = item.type();
-    if (type.empty()) {
-      *error =
-          "The type child element of datacontrol element is obligatory";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool MetadataValidation(
-    const UIApplicationSingleEntry& it,
-    std::string* error) {
-  for (const auto& item : it.meta_data) {
-    if (item.key().empty()) {
-      *error =
-          "The key child element of metadata element is obligatory";
-      return false;
-    }
-  }
-  return true;
-}
-
-bool LabelValidation(const UIApplicationSingleEntry& it, std::string* error) {
-  for (const auto& item : it.label) {
-    if (item.text().empty()) {
-      *error = "The text child element of label element is obligatory";
-      return false;
-    }
-
-    const std::string& name = item.name();
-    if (name.empty()) {
-      *error =
-          "The name child element of label element is obligatory";
-      return false;
-    }
   }
   return true;
 }
@@ -458,32 +191,32 @@ bool ParseUIApplicationAndStore(
     std::string* error) {
   std::string appid;
   if (app_dict.GetString(kUIApplicationAppIDKey, &appid))
-    uiapplicationinfo->ui_info.set_appid(appid);
+    uiapplicationinfo->app_info.set_appid(appid);
   std::string exec;
   if (app_dict.GetString(kUIApplicationExecKey, &exec))
-    uiapplicationinfo->ui_info.set_exec(exec);
+    uiapplicationinfo->app_info.set_exec(exec);
   std::string multiple;
   if (app_dict.GetString(kUIApplicationMultipleKey, &multiple))
-    uiapplicationinfo->ui_info.set_multiple(multiple);
+    uiapplicationinfo->app_info.set_multiple(multiple);
   std::string nodisplay;
   if (app_dict.GetString(kUIApplicationNoDisplayKey, &nodisplay))
-    uiapplicationinfo->ui_info.set_nodisplay(nodisplay);
+    uiapplicationinfo->app_info.set_nodisplay(nodisplay);
   std::string taskmanage;
   if (app_dict.GetString(kUIApplicationTaskManageKey, &taskmanage))
-    uiapplicationinfo->ui_info.set_taskmanage(taskmanage);
+    uiapplicationinfo->app_info.set_taskmanage(taskmanage);
   std::string type;
   if (app_dict.GetString(kUIApplicationTypeKey, &type))
-    uiapplicationinfo->ui_info.set_type(type);
+    uiapplicationinfo->app_info.set_type(type);
   std::string uigadget;
   if (app_dict.GetString(kUIApplicationUIGadgetKey, &uigadget))
-    uiapplicationinfo->ui_info.set_uigadget(uigadget);
+    uiapplicationinfo->app_info.set_uigadget(uigadget);
   std::string process_pool;
   if (app_dict.GetString(kUIApplicationProcessPoolKey, &process_pool)) {
     if (!IsBooleanString(process_pool)) {
       *error = "process_pool must be 'true' or 'false'";
       return false;
     }
-    uiapplicationinfo->ui_info.set_process_pool(process_pool);
+    uiapplicationinfo->app_info.set_process_pool(process_pool);
   }
   std::string submode;
   if (app_dict.GetString(kUIApplicationSubmodeKey, &submode)) {
@@ -491,11 +224,11 @@ bool ParseUIApplicationAndStore(
       *error = "submode must be 'true' or 'false'";
       return false;
     }
-    uiapplicationinfo->ui_info.set_submode(submode);
+    uiapplicationinfo->app_info.set_submode(submode);
   }
   std::string submode_mainid;
   if (app_dict.GetString(kUIApplicationSubmodeMainIDKey, &submode_mainid))
-    uiapplicationinfo->ui_info.set_submode_mainid(submode_mainid);
+    uiapplicationinfo->app_info.set_submode_mainid(submode_mainid);
   std::string indicator_display;
   if (app_dict.GetString(kUIApplicationIndicatorDisplayKey,
                          &indicator_display)) {
@@ -503,19 +236,19 @@ bool ParseUIApplicationAndStore(
       *error = "indicatordisplay must be 'true' or 'false'";
       return false;
     }
-    uiapplicationinfo->ui_info.set_indicator_display(indicator_display);
+    uiapplicationinfo->app_info.set_indicator_display(indicator_display);
   }
   std::string portrait_effectimage;
   if (app_dict.GetString(kUIApplicationPortraitEffectImageKey,
                      &portrait_effectimage))
-    uiapplicationinfo->ui_info.set_portrait_image(portrait_effectimage);
+    uiapplicationinfo->app_info.set_portrait_image(portrait_effectimage);
   std::string landscape_effectimage;
   if (app_dict.GetString(kUIApplicationLandscapeEffectImageKey,
                      &landscape_effectimage))
-    uiapplicationinfo->ui_info.set_landscape_image(landscape_effectimage);
+    uiapplicationinfo->app_info.set_landscape_image(landscape_effectimage);
   std::string effectimage_type;
   if (app_dict.GetString(kUIApplicationEffectImageTypeKey, &effectimage_type))
-    uiapplicationinfo->ui_info.set_effectimage_type(effectimage_type);
+    uiapplicationinfo->app_info.set_effectimage_type(effectimage_type);
 
   std::string launch_mode;
   if (app_dict.GetString(kUIApplicationLaunchModeKey, &launch_mode)) {
@@ -523,12 +256,12 @@ bool ParseUIApplicationAndStore(
       *error = "launch_mode attribute is empty";
       return false;
     }
-    uiapplicationinfo->ui_info.set_launch_mode(launch_mode);
+    uiapplicationinfo->app_info.set_launch_mode(launch_mode);
   }
 
   std::string hwacceleration;
   if (app_dict.GetString(kUIApplicationHwAccelerationKey, &hwacceleration))
-    uiapplicationinfo->ui_info.set_hwacceleration(hwacceleration);
+    uiapplicationinfo->app_info.set_hwacceleration(hwacceleration);
 
   return InitializeParsing(app_dict, uiapplicationinfo, error);
 }
@@ -569,7 +302,6 @@ bool UIApplicationHandler::Validate(
     std::string* error) const {
   const UIApplicationInfoList& elements =
       static_cast<const UIApplicationInfoList&>(data);
-
 
   std::shared_ptr<const PackageInfo> package_info =
       std::static_pointer_cast<const PackageInfo>(
