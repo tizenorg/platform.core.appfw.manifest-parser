1ee43666bff146e49aee2619f381a98e0e0e9aea
Lukasz Wysocki <l.wysocki@samsung.com>
2015-12-15 10:49:46 +0100

    Add background-category parsing from metadata element
    Refactoring UI, Service and Widget Application Handlers
    
    Change-Id: I65d5617f80a2f426ca297b5ca9ad22ce910c2262

--- src/manifest_parser/utils/w3c_languages.h
+++ src/manifest_parser/utils/w3c_languages.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by an apache 2.0 license that can be
 // found in the LICENSE file.
 
-#ifndef UTILS_W3C_LANGUAGES_H_
-#define UTILS_W3C_LANGUAGES_H_
+#ifndef MANIFEST_PARSER_UTILS_W3C_LANGUAGES_H_
+#define MANIFEST_PARSER_UTILS_W3C_LANGUAGES_H_
 
 #include <map>
 #include <string>
@@ -35,4 +35,4 @@ extern const std::unordered_set<std::string> kW3CVariant;
 }  // namespace w3c_languages
 }  // namespace utils
 
-#endif  // UTILS_W3C_LANGUAGES_H_
+#endif  // MANIFEST_PARSER_UTILS_W3C_LANGUAGES_H_
