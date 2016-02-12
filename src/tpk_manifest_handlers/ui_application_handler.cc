452014526dd7906e9f070c317aeb28096034bae8
Hwankyu Jhun <h.jhun@samsung.com>
2016-02-12 20:21:02 +0900

    Support splash screen
    Change-Id: I953fc26d01549cacf5752e5fc5140db13682bb19
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

--- src/tpk_manifest_handlers/ui_application_handler.cc
+++ src/tpk_manifest_handlers/ui_application_handler.cc
@@ -87,6 +87,10 @@ bool InitializeParsing(const parser::DictionaryValue& app_dict,
   if (!InitializeParsingElement(app_dict, tpk_app_keys::kBackgroundCategoryKey,
       parsingFunc, uiapplicationinfo, error))
     return false;
+  parsingFunc = ParseSplashScreen<UIApplicationSingleEntry>;
+  if (!InitializeParsingElement(app_dict, tpk_app_keys::kSplashScreensKey,
+      parsingFunc, uiapplicationinfo, error))
+    return false;
 
   return true;
 }
