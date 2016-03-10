cbb132142a2050b0f56563397618e09f22b6724a
Tomasz Iwanek <t.iwanek@samsung.com>
2016-03-10 10:12:42 +0100

    Remove author email and href validation
    We cannot translate authors email and href into valid one for platform
    manifest if they don't exist, therefore for now validation of author is
    removed as it is causing trouble when updating wgt package with author
    but without email or href.
    
    Change-Id: I70af08ec498fb1234b9d3dea058baae964b82ce3

--- src/tpk_manifest_handlers/author_handler.cc
+++ src/tpk_manifest_handlers/author_handler.cc
@@ -58,42 +58,7 @@ bool AuthorHandler::Parse(
 
   ParseAuthorAndStore(*items[0], author.get());
 
-  // TODO(t.iwanek): don't skip it when preloaded apps will be fixed.
-  if (author->name().empty())
-    return true;
-
   *output = std::static_pointer_cast<parser::ManifestData>(author);
-  return true;
-}
-
-bool AuthorHandler::Validate(
-    const parser::ManifestData& data,
-    const parser::ManifestDataMap& /*handlers_output*/,
-    std::string* error) const {
-  const AuthorInfo& author =
-      static_cast<const AuthorInfo&>(data);
-
-  if (author.email().empty()) {
-    *error = "The email child element of author element is obligatory";
-    return false;
-  }
-
-  // TODO(t.iwanek): validate email address when preloaded apps will be fixed.
-
-
-  const std::string& href = author.href();
-  if (href.empty()) {
-    *error =
-        "The href child element of author element is obligatory";
-    return false;
-  }
-
-  const std::string& name = author.name();
-  if (name.empty()) {
-    *error =
-        "The name child element of author element is obligatory";
-    return false;
-  }
   return true;
 }
 
